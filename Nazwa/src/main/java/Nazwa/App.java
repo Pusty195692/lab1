0c9c28404ff081b840880c4b44b8058ce56d4f62
Adrian <195692@edu.p.lodz.pl>
2017-03-08 08:57:24 +0100

    trzeci commit

--- Nazwa/src/main/java/Nazwa/App.java
+++ Nazwa/src/main/java/Nazwa/App.java
@@ -4,22 +4,17 @@ package Nazwa;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
-<<<<<<< HEAD
-	
-	public static void metodaAdriana() {
-		
-	}	
- }
-=======
-	public void adaMethod()
-	{
-		System.out.println("konflikty, wszędzie konflikty");
-	}
+
+    public static void metodaAdriana() {
+        
+    }
+
+    public void adaMethod() {
+        System.out.println("konflikty, wszędzie konflikty");
+    }
 }
->>>>>>> e2ec3f24a66114a725cbe646c1eaf841301058b2
